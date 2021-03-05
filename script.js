0ff2f61e8f1ac552f0ba9817e5f908600b5c2648
Zuzanna <zuzannamoskala@gmail.com>
2021-03-05 20:36:07 +0100

    challenge 3

--- script.js
+++ script.js
@@ -4,21 +4,19 @@
 // Coding Challenge #1
 
 
- // We're building a football betting app (soccer for my American friends 😅)!
- //
- // Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
- //
- // 1. Create one player array for each team (variables 'players1' and 'players2')
- // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
- // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
- // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
- // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
- // 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
- // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
- //
- // TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
- //
- // GOOD LUCK 😀
+// We're building a football betting app (soccer for my American friends 😅)!
+//
+// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your
+// tasks:  1. Create one player array for each team (variables 'players1' and 'players2') 2. The first player in any player array is the
+// goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one
+// array ('fieldPlayers') with all the remaining 10 field players 3. Create an array 'allPlayers' containing all players of both teams (22
+// players) 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all
+// the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic' 5. Based on the game.odds object, create one variable for each odd
+// (called 'team1', 'draw' and 'team2') 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
+// and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in) 7.
+// The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else
+// statement or the ternary operator.  TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the
+// function again with players from game.scored  GOOD LUCK 😀
 
 //
 // const game = {
@@ -126,120 +124,170 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 GOOD LUCK 😀
 */
 
-let game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+// let game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+//
+// //1
+// for(const [i, scored] of game.scored.entries()) {
+//   console.log(`Goal ${i}: ${scored}`);
+// }
+//
+// //2
+// let sum = 0;
+// const gameOdds = Object.values(game.odds);
+//
+// for (const odd of gameOdds){
+//   sum+=odd;
+//   console.log(sum);
+// }
+//
+// console.log(sum/gameOdds.length);
+//
+// //3
+//
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   console.log(team);
+//   const teamStr = team === 'x' ? 'draw' : `victory of ${game[team]}`;
+//   console.log(`Odd of victory of ${teamStr} : ${odd}`);
+// }
+//
+// console.log(game.team1);
+//
+// //BONUS
+// const scorers = {};
+//
+// for (const player of game.scored) {
+//   console.log(player);
+//   scorers[player] ? scorers[player]++ : scorers[player] = 1;
+// }
+//
+// game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   scorers,
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card']
+]);
 
 //1
-for(const [i, scored] of game.scored.entries()) {
-  console.log(`Goal ${i}: ${scored}`);
-}
+const set = new Set(gameEvents.values());
+console.log(set);
+const events = [...set];
+console.log(events);
 
 //2
-let sum = 0;
-const gameOdds = Object.values(game.odds);
-
-for (const odd of gameOdds){
-  sum+=odd;
-  console.log(sum);
-}
-
-console.log(sum/gameOdds.length);
+gameEvents.delete(64);
+console.log(gameEvents);
 
 //3
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
 
-for (const [team, odd] of Object.entries(game.odds)) {
-  console.log(team);
-  const teamStr = team === 'x' ? 'draw' : `victory of ${game[team]}`;
-  console.log(`Odd of victory of ${teamStr} : ${odd}`);
+//4
+for (const [key,value] of gameEvents) {
+  const str = key <= 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+  console.log(`${str} ${key} : ${value}`);
 }
-
-console.log(game.team1);
-
-//BONUS
-const scorers = {};
-
-for (const player of game.scored) {
-  console.log(player);
-  scorers[player] ? scorers[player]++ : scorers[player] = 1;
-}
-
-game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  scorers,
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
