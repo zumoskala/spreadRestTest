59555c6c95b1684020bdba19b7acc7f58766c4f3
Zuzanna <zuzannamoskala@gmail.com>
2021-03-06 21:24:29 +0100

    challenge 4

--- script.js
+++ script.js
@@ -294,10 +294,63 @@ GOOD LUCK 😀
 
 
 //STRING METHODS - mask
-const str = 'ziemniaczek prosiaczek';
-console.log(str.length);
+// const str = 'ziemniaczek prosiaczek';
+// console.log(str.length);
+//
+// const last = str.slice(-10);
+// console.log(last, last.length);
+//
+// console.log(last.padStart(str.length, '*'));
 
-const last = str.slice(-10);
-console.log(last, last.length);
 
-console.log(last.padStart(str.length, '*'));
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below),
+and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+
+ */
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function() {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const stripped = row.trim();
+    let  splitted = stripped.split("_");
+    splitted = [splitted[0].toLowerCase(), splitted[1].toLowerCase()];
+    splitted[1] = splitted[1][0].toUpperCase() + splitted[1].slice(1);
+    const final = splitted[0].concat(splitted[1]);
+    console.log(final, `${'v'.repeat(i+1)}`);
+  }
+
+});
